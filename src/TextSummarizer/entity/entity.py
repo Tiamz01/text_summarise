7ceaeaae76b763dc755694b98ab6d91b24fefa8e
tiamz <tiami.abiola@gmail.com>
2024-03-16 17:25:06 +0100

    updating model trainer module

--- src/TextSummarizer/entity/entity.py
+++ src/TextSummarizer/entity/entity.py
@@ -51,3 +51,12 @@ class ModelTrainerConfig:
     eval_steps: int 
     save_steps: float
     gradient_accumulation_steps: int
+
+
+@dataclass(frozen=True)
+class ModelEvationConfig:
+    root_dir: Path
+    data_path: Path
+    model_path: Path
+    tokenizer_path: Path
+    metric_file_name: Path
